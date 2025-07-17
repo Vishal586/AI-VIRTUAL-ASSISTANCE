0b21d0b904c94934aa5e3f85106c9583a3f45184
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2025-07-17 18:32:27 +0530

    Add files via upload

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,57 @@
+// src/context/userContext.js
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react';
+
+export const userDataContext = createContext();
+
+function UserContext({ children }) {
+    const [frontendImage, setFrontendImage] = useState(null)
+    const [backendImage, setBackendImage] = useState(null)
+    const [userData, setUserData] = useState(null);
+    const serverUrl = "http://localhost:8000";
+    const [selectedImage, setSelectedImage] = useState(null)
+
+    const handleCurrentUser = async () => {
+        try {
+            const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true })
+            setUserData(result.data)
+            console.log(result.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const getGeminiResponse = async (command) => {
+        try {
+            const result = await axios.post(`${serverUrl}/api/user/asktoassistant`, { command }, { withCredentials: true })
+            return result.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        handleCurrentUser()
+    }, [])
+
+    const value = {
+        serverUrl,
+        userData,
+        setUserData,
+        backendImage,
+        setBackendImage,
+        frontendImage,
+        setFrontendImage,
+        selectedImage,
+        setSelectedImage,
+        getGeminiResponse
+    };
+
+    return (
+        <userDataContext.Provider value={value}>
+            {children}
+        </userDataContext.Provider>
+    );
+}
+
+export default UserContext;
