7dc7d8be0d98502d4009b22e20ddd431c7c9ac90
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2025-07-17 19:04:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-virtual-assistance.onrender.com",
     credentials:true
 }))
 
